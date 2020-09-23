cd88816d6085cfa0c3d3dbae251fc3ba3fa665f6
Natthaphon Phanit <kekhuay@users.noreply.github.com>
2020-09-23 11:29:54 +0700

    created serenity.conf

--- src/test/kotlin/com/nexterdigitals/nextershop/automationtest/CucumberTestSuite.kt
+++ src/test/kotlin/com/nexterdigitals/nextershop/automationtest/CucumberTestSuite.kt
@@ -5,8 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity
 import org.junit.runner.RunWith
 
 @RunWith(CucumberWithSerenity::class)
-@CucumberOptions(
-    plugin = ["pretty"],
-    features = ["src/test/resources/features"]
-)
+@CucumberOptions(plugin = ["pretty"], features = ["src/test/resources/features"])
 class CucumberTestSuite
