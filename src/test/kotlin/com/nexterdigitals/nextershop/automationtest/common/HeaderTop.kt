011a89accc8db5ddf781346cf2db82136d512b78
Natthaphon Phanit <kekhuay@users.noreply.github.com>
2020-10-31 23:18:22 +0700

    create environment specific property

--- src/test/kotlin/com/nexterdigitals/nextershop/automationtest/common/HeaderTop.kt
+++ src/test/kotlin/com/nexterdigitals/nextershop/automationtest/common/HeaderTop.kt
@@ -3,6 +3,5 @@ package com.nexterdigitals.nextershop.automationtest.common
 import org.openqa.selenium.By
 
 object HeaderTop {
-    val USER_PROFILE_ICON: By = By.cssSelector("#user-profile-icon")
-    val DISPLAYED_NAME: By = By.className("display-name")
+    val USER_PROFILE_ICON: By = By.cssSelector(".p-l-18")
 }
--- src/test/kotlin/com/nexterdigitals/nextershop/automationtest/navigation/NextershopHomePage.kt
+++ src/test/kotlin/com/nexterdigitals/nextershop/automationtest/navigation/NextershopHomePage.kt
@@ -3,5 +3,5 @@ package com.nexterdigitals.nextershop.automationtest.navigation
 import net.serenitybdd.core.pages.PageObject
 import net.thucydides.core.annotations.DefaultUrl
 
-@DefaultUrl(value = "https://nextershop.com")
+@DefaultUrl(value = "https://nexter-vip:GNAAa4oYfYb1jP8!%40%40e8sN%5E%2390r%25RDc9@dev.nextershop.com")
 class NextershopHomePage : PageObject()
